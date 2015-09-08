b605e4fd7bab8fff62b2e09b95040551e7226e96
Vitalii Chechel <Vitalii_Chechel@epam.com>
2015-09-08 17:31:48 +0300

    remove extra controllers

--- src/main/java/com/ticket/controllers/admin/LoadData.java
+++ src/main/java/com/ticket/controllers/admin/LoadData.java
@@ -21,32 +21,6 @@ public class LoadData {
 
     @Autowired
     StationService stationService;
-//    @Autowired
-//    TrainService trainService;
-
-//    @RequestMapping(value = "/booking", method = RequestMethod.GET)
-//    @Consumes("application/json")
-//    public String updateStations(ModelMap model) {
-//        long startTime = System.currentTimeMillis();
-//        Collection<BookingStation> stations = stationService.loadBookingStations();
-//        long stopTimeInSec = (System.currentTimeMillis() - startTime) / 1000;
-//        model.addAttribute("stations", stations);
-//        model.addAttribute("countStations", stations.size());
-//        model.addAttribute("timeInSec", stopTimeInSec);
-//        return "load-stations";
-//    }
-//
-//    @RequestMapping(value = "/uz", method = RequestMethod.GET)
-//     @Consumes("application/json")
-//     public String updateStationsUZ(ModelMap model) {
-//        long startTime = System.currentTimeMillis();
-//        Collection<UZStation> stations = stationService.loadUZStations();
-//        long stopTimeInSec = (System.currentTimeMillis() - startTime) / 1000;
-//        model.addAttribute("stations", stations);
-//        model.addAttribute("countStations", stations.size());
-//        model.addAttribute("timeInSec", stopTimeInSec);
-//        return "load-stations";
-//    }
 
     @RequestMapping(value = "/st", method = RequestMethod.GET)
     public String loadStations(ModelMap model) {
